9668ffd56b1b84b40b3955f3f498909b430b018f
Cesar Augusto <68303891+moviessjk@users.noreply.github.com>
2023-04-26 22:52:12 +0000

    variavel dentro do metodo

--- dicionario.py
+++ dicionario.py
@@ -3,26 +3,19 @@ def dict_to_string(dicionario):
     resultado = ", ".join(lista)
     return resultado
 
-meu_dicionario = {
-    'nome': 'João',
-    'idade': 30,
-    'cidade': 'São Paulo'
-}
 
 
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-meu_dicionario = {
-    'nome': 'Joao',
-    'idade': 30,
-    'cidade': 'Sao Paulo'
-}
-
 @app.route('/dicionario')
 def get_dicionario():
-    
+    meu_dicionario = {
+        'nome': 'Joao',
+        'idade': 30,
+        'cidade': 'Sao Paulo'
+    }
     resultado = dict_to_string(meu_dicionario)
     return jsonify(resultado)
 
