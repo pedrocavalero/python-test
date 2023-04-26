ffc2a839f2b4cb2ed032c6bcda4d2bd2f3934a25
Cesar Augusto <68303891+moviessjk@users.noreply.github.com>
2023-04-26 22:16:14 +0000

    criando_dicionario

---
+++ dicionario.py
@@ -0,0 +1,33 @@
+def dict_to_string(dicionario):
+    lista = [f"{chave}: {valor}" for chave, valor in dicionario.items()]
+    resultado = ", ".join(lista)
+    return resultado
+
+meu_dicionario = {
+    'nome': 'João',
+    'idade': 30,
+    'cidade': 'São Paulo'
+}
+
+string_resultado = dict_to_string(meu_dicionario)
+print(string_resultado)
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+meu_dicionario = {
+    'nome': 'Joao',
+    'idade': 30,
+    'cidade': 'Sao Paulo'
+}
+
+@app.route('/dicionario')
+def get_dicionario():
+    
+    resultado = dict_to_string(meu_dicionario)
+    return jsonify(resultado)
+
+
+if __name__ == '__main__':
+    app.run()
