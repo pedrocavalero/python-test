ee05bff1a1a245ce2183561140d5aabed871f6ec
Cesar Augusto <68303891+moviessjk@users.noreply.github.com>
2023-04-26 23:06:46 +0000

    transferi metodo para app

--- dicionario.py
+++ dicionario.py
@@ -2,21 +2,3 @@ def dict_to_string(dicionario):
     lista = [f"{chave}: {valor}" for chave, valor in dicionario.items()]
     resultado = ", ".join(lista)
     return resultado
-
-
-
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/dicionario')
-def get_dicionario():
-    meu_dicionario = {
-        'nome': 'Joao',
-        'idade': 30,
-        'cidade': 'Sao Paulo'
-    }
-    resultado = dict_to_string(meu_dicionario)
-    return jsonify(resultado)
-
-
