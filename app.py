380792e9542f0bdd6e9aed01ceb1660ed371a298
Cesar Augusto <68303891+moviessjk@users.noreply.github.com>
2023-05-03 22:54:56 +0000

    corrigido

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, jsonify
 from dicionario import dict_to_string
-from flask import Flask, jsonify
-from dicionario import dict_to_string
 
 app = Flask(__name__)
 
@@ -9,7 +7,21 @@ app = Flask(__name__)
 def hello():
     return "Hello Cesar"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/ola/<name>")
+def Ola(name):
+    return f"Ola, {name}!"
+
+@app.route('/dicionario')
+def get_dicionario():
+    meu_dicionario = {
+        'nome': 'Joao',
+        'idade': 30,
+        'cidade': 'Sao Paulo'
+    }
+    resultado = dict_to_string(meu_dicionario)
+    return jsonify(resultado)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
