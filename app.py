0a05cfdbb5d143e72a17273152c5aa4fc66f408c
Cesar Augusto <68303891+moviessjk@users.noreply.github.com>
2023-05-17 22:26:52 +0000

    lista atualizada

--- app.py
+++ app.py
@@ -15,5 +15,19 @@ def get_itens():
     itens_filtrados = [item for item in itens if (cor is None or item['cor'] == cor) and (tamanho is None or item['tamanho'] == tamanho)]
     return jsonify(itens_filtrados)
 
+
+@app.route('/concatenar')
+def concatenar():
+    parametros = request.args.getlist('parametro')
+    lista_parametros = list(parametros)
+    string_concatenada = ''
+    for parametro in lista_parametros:
+        string_concatenada += parametro + '-'
+
+    string_concatenada = string_concatenada[:-1]
+
+    return f'A lista de parâmetros é: {lista_parametros}. A string concatenada é: {string_concatenada}.'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
