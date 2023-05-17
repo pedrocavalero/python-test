33494373b8575b7a6771076c89bc56a4cd315fc2
Cesar Augusto <68303891+moviessjk@users.noreply.github.com>
2023-05-17 21:52:24 +0000

    corrigido update

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 from flask import Flask, jsonify, request
-from dicionario import dict_to_string
-from itens import get_itens
+from itens import itens
 
 app = Flask(__name__)
-
-@app.route("/hello/<name>")
-def hello(name):
-    return f"Hello {name}!"
 
 @app.route('/lista')
 def lista():
     lista=['maca', 'macaco', 'chuchu']
     return lista
-from flask import Flask, jsonify, request
-
 
 @app.route('/itens')
 def get_itens():
@@ -21,21 +14,6 @@ def get_itens():
     tamanho = request.args.get('tamanho')
     itens_filtrados = [item for item in itens if (cor is None or item['cor'] == cor) and (tamanho is None or item['tamanho'] == tamanho)]
     return jsonify(itens_filtrados)
-
-
-
-
-@app.route('/dicionario')
-def get_dicionario():
-    meu_dicionario = {
-        'nome': 'Joao',
-        'idade': 30,
-        'cidade': 'Sao Paulo'
-    }
-    resultado = dict_to_string(meu_dicionario)
-    return jsonify(resultado)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
